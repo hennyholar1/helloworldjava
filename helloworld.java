458ee3d6fdad7f366e860d59c295220e196d2b37
hennyholar1 <hennyholar1@users.noreply.github.com>
2022-02-28 00:06:25 -0600

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World! Now the test will be pulled every minute. Now changing it from minute-pull, which is wasting our resources, to use webhooks to trigger the jenkins job.");
+    System.out.println("Hello World! Modified to validate if webhooks will trigger the jenkins job.");
     }
 }
